67c1554fea249d66b3819a09efa27b992d61a66c
David Muñoz <43390350+davidmubernal@users.noreply.github.com>
2021-03-05 13:32:37 +0000

    Testing (#3)
    * Update test_2.py
    
    * Update test_2.py
    
    * Update test_2.py - Automated PEP 8 check

--- test_2.py
+++ test_2.py
@@ -1,11 +1,10 @@
 print('Hello world 2')
-print ('check autopep 8')
+print('check autopep 8')
 
 #this sould be bad
 
 # this could be good
 
-
 # delete line before this line
 
-print ('testing again' )
+print('testing again')
